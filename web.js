7a205ff177730e8ec5277676a69997b6bfe4c899
unknown <refra2n@naver.com>
2026-03-17 17:10:26 +0900

    Update port to 8002

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT || 8001;
+var PORT = process.env.PORT || 8002;
 
 app.use(express.static(path.join(__dirname, "dist")));
 
